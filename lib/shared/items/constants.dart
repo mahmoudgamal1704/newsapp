c938936e7a369138984e535c39b2234090fb4df3
mahmoud <mahmoudgamal1704@gmail.com>
2022-12-25 15:25:17 +0200

    add http package and get response

---
+++ lib/shared/items/constants.dart
@@ -0,0 +1,9 @@
+
+
+
+const String BASE = 'newsapi.org';
+const String APIKEY = '7bdc9c4aab1143f5ac448443e2f6ec0c';
+
+const String SourcesEndPoint = '/v2/top-headlines/sources';
+
+const String NewsEndPoint = '/v2/everything';
---
+++ lib/shared/network/remote/apimanager.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:newsapp/shared/items/constants.dart';
+
+class ApiManage {
+
+   Future<void> getSources() async {
+      Uri URL = Uri.https(BASE,SourcesEndPoint);
+     Response response = await http.get(URL);
+     var json = jsonDecode(response.body);
+
+   }
+}
